0582ef5da6d6e542b9d159a5ae8f9eba4e961406
sneha_sharma <sneha902440@gmail.com>
2025-10-15 13:42:17 +0530

    Update CompletionBadeNFT.sol

--- CompletionBadeNFT.sol
+++ CompletionBadeNFT.sol
@@ -1,1 +1,81 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract CompletionBadgeNFT {
+    string public name = "CompletionBadgeNFT";
+    string public symbol = "CBNFT";
+
+    address public owner;
+    uint256 public totalSupply;
+
+    mapping(uint256 => address) private _owners;
+    mapping(address => uint256) private _balances;
+    mapping(uint256 => address) private _tokenApprovals;
+
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function mint() public onlyOwner {
+        uint256 tokenId = totalSupply + 1;
+        totalSupply = tokenId;
+
+        _owners[tokenId] = msg.sender;
+        _balances[msg.sender] += 1;
+
+        emit Transfer(address(0), msg.sender, tokenId);
+    }
+
+    function balanceOf(address user) public view returns (uint256) {
+        require(user != address(0), "Zero address not valid");
+        return _balances[user];
+    }
+
+    function ownerOf(uint256 tokenId) public view returns (address) {
+        address tokenOwner = _owners[tokenId];
+        require(tokenOwner != address(0), "Token does not exist");
+        return tokenOwner;
+    }
+
+    function transferFrom(address from, address to, uint256 tokenId) public {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Not approved");
+        require(ownerOf(tokenId) == from, "Not token owner");
+        require(to != address(0), "Transfer to zero address");
+
+        _approve(address(0), tokenId);
+
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    function approve(address to, uint256 tokenId) public {
+        address tokenOwner = ownerOf(tokenId);
+        require(msg.sender == tokenOwner, "Not token owner");
+        _approve(to, tokenId);
+    }
+
+    function getApproved(uint256 tokenId) public view returns (address) {
+        require(_owners[tokenId] != address(0), "Token does not exist");
+        return _tokenApprovals[tokenId];
+    }
+
+    function _approve(address to, uint256 tokenId) private {
+        _tokenApprovals[tokenId] = to;
+    }
+
+    function _isApprovedOrOwner(address spender, uint256 tokenId) private view returns (bool) {
+        address tokenOwner = ownerOf(tokenId);
+        return (spender == tokenOwner || getApproved(tokenId) == spender);
+    }
+}
 
